3496ac7fd52a06eea7967226ebaed482e9805bac
vikramguhilot <guhilot@pdx.edu>
2020-05-08 16:21:15 -0700

    EX6REDO

--- assign3/6-background.js
+++ assign3/6-background.js
@@ -10,8 +10,7 @@ function random_rgba() {
 const btn = document.getElementById('btn1')
 btn.onclick = function(e){
     e.preventDefault()
-    //const value1 = btn.className == "btn btn-danger"? document.getElementById("interval").value : 500
-    //document.getElementById("interval").value
+
     console.log(btn.className)
 
     const value1 = document.getElementById("interval").value
@@ -19,29 +18,20 @@ btn.onclick = function(e){
 
     document.querySelector('.btn').value = document.querySelector('.btn').value == 'Stop'? 'Start':'Stop'
     btn.className = btn.className == "btn btn-danger"? "btn btn-primary": "btn btn-danger" 
-    
-        let hold = setInterval(()=>{
+
+
+    btn.className == "btn btn-danger"? hold = setInterval(()=>{
         document.body.style.backgroundColor = random_rgba()
-        }, value1*1000)
-
+        }, value1*1000) : clearInterval(hold)
 }
-
- 
-    //document.querySelector('.btn').value = 'Hide'
 
 
 function random_rgba() {
     var o = Math.round, r = Math.random, s = 255;
     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
 }
-//document.querySelector('.btn').value = 'Hide'
 const body = document.getElementsByTagName('body')
 
-
-// setInterval(()=>{
-//     document.body.style.backgroundColor = random_rgba()
-// }, 10000)
-//class="btn btn-primary"
 
 
 
