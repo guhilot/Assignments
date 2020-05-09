2e54c0a602bfb18e23493f327152a96ccfb42366
vikramguhilot <guhilot@pdx.edu>
2020-05-09 13:21:41 -0700

    EX4

--- assign3/4-regex.js
+++ assign3/4-regex.js
@@ -1,1 +1,28 @@
 // Enter your code here
+// let sample = "Whitespace is important in separating words";
+// let countWhiteSpace = /[^\s]+\s/g; // Change this line
+// let result = sample.match(countWhiteSpace);
+// console.log(result);
+
+const form = document.getElementById("frm")
+
+
+form.onsubmit = function(e){
+    e.preventDefault()
+    const butt = document.getElementById("butt")
+    const text = document.getElementById("userinput").value
+    //console.log(text)
+    
+
+    let snip = /[^,]+,\s/g
+    let result = text.match(snip)
+
+    for(i=0;i<result.length;++i){
+        const p = document.createElement('p')
+        const hr = document.createElement('hr')
+        butt.append(p)
+        butt.append(hr)
+        p.innerHTML = result[i].slice(0, -2)
+        
+    }
+}
