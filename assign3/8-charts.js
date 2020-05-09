09e743ef246f8e60c16bc132203a239b4bf5cfbe
vikramguhilot <guhilot@pdx.edu>
2020-05-08 18:03:08 -0700

    EX8

--- assign3/8-charts.js
+++ assign3/8-charts.js
@@ -9,6 +9,70 @@ let data = {
   orange: 9,
   unknown: 4,
 };
+const url = 'https://swapi.dev/api/people';
+fetch(url)
+        .then(res=>res.json())
+        .then(data=>dataMan([data]))
+
+        function dataMan(val){
+          const arr = val.map(data=>{
+            return data.results.map(info=>{
+              return {y: Number(info.height), label: info.name}
+            })
+          })
+          console.log(arr)
+          const r = arr[0]
+          console.log(r)
+          var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            title:{
+              text: "Height Comparason",
+              horizontalAlign: "left"
+            },
+            data: [{
+              type: "doughnut",
+              startAngle: 60,
+              //innerRadius: 60,
+              indexLabelFontSize: 17,
+              indexLabel: "{label} - #percent%",
+              toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+              dataPoints: r
+            }]
+          });
+          chart.render();
+        }
+
+// window.onload = function () {
+
+//   const arr = [
+//     { y: 67, label: "Inbox" },
+//     { y: 28, label: "Archives" },
+//     { y: 10, label: "Labels" },
+//     { y: 7, label: "Drafts"},
+//     { y: 15, label: "Trash"},
+//     { y: 6, label: "Spam"}
+//   ]
+ 
+//   var chart = new CanvasJS.Chart("chartContainer", {
+//     animationEnabled: true,
+//     title:{
+//       text: "Email Categories",
+//       horizontalAlign: "left"
+//     },
+//     data: [{
+//       type: "doughnut",
+//       startAngle: 60,
+//       //innerRadius: 60,
+//       indexLabelFontSize: 17,
+//       indexLabel: "{label} - #percent%",
+//       toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+//       dataPoints: arr
+//     }]
+//   });
+//   chart.render();
+//   }
+
+
 
 // Colors used :
 
