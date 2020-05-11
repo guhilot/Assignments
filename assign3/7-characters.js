32e89f8a5ae8f722adaf22aac629d044b2b3f251
vikramguhilot <guhilot@pdx.edu>
2020-05-10 21:18:34 -0700

    modify

--- assign3/7-characters.js
+++ assign3/7-characters.js
@@ -1,6 +1,71 @@
 // Enter your code here
 
-const url = 'https://swapi.dev/api/people';
+// const getCharacters = async function(pageNo = 1)
+// {
+//     const url = 'https://swapi.dev/api/people/'
+
+//     let actualUrl = url + `?page=${pageNo}`
+
+//     var apiResults= await fetch(actualUrl)
+//         .then(res=>res.json())
+//     return apiResults
+// }
+
+// const getEntireCharacterList = async function(pageNo = 1)
+// {
+//     const result = await getCharacters(pageNo)
+//     const finalres = result.results 
+//         if(result.next != null){
+//             return finalres.concat(await getEntireCharacterList(pageNo+1))
+//         }
+//         else{
+//             return finalres
+//         }
+// }
+
+
+
+//     (async ()=>{
+//         const entireList = await getEntireCharacterList()
+//         var ul = document.getElementById('results')
+//         for(let i = 0; i < entireList.length ; ++i)
+//         {
+//             var li = document.createElement('li')
+//             li.innerHTML = entireList[i].name + ' - ' + entireList[i].birth_year
+//             ul.appendChild(li)
+//         }
+//     })();
+
+// const url = 'https://swapi.dev/api/people/'
+
+
+// const hold = async function(url){
+//     var apiResults= await fetch(url)
+//         .then(res=>res.json())
+//     return apiResults
+// }
+
+// const val = hold("https://swapi.dev/api/people")
+
+for(i = 1; i < 10; ++i)
+{
+    const url = 'https://swapi.dev/api/people/?page=' + i.toString();
+    //console.log(url)
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>dataMan([data]))
+}
+
+// function dataMan(val){
+//     const oo = val.map(nom=>{
+//         return nom.results.map(n=>{
+//             return {name : n.name, birthday : n.birth_year}
+//         })
+//     })
+//     console.log(oo)
+// }
+
+const url = 'https://swapi.dev/api/people/?page=3';
 
    function dataMan(val){
        const h2 = document.querySelector('h2')
@@ -20,19 +85,3 @@ const url = 'https://swapi.dev/api/people';
             })
        })
     }
-    //dataMan([data])
-
-    async function loop(url){
-        // url1 = url.slice(0, -1)
-        console.log(url.toString())
-        // for(i=2; i<=9; ++i)
-        // {
-           await fetch(url)
-            .then(res=>res.json())
-            .then(data=>console.log(data))
-        // } 
-    }
-  
-    fetch(url)
-        .then(res=>res.json())
-        .then(data=>dataMan([data]))
