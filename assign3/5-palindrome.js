3f0aad744f33c3e160dddf7422c26f5f7e8c0398
vikramguhilot <guhilot@pdx.edu>
2020-05-10 18:04:49 -0700

    redo

--- assign3/5-palindrome.js
+++ assign3/5-palindrome.js
@@ -1,8 +1,23 @@
 // Enter your code here
 
 const form = document.getElementById("frm")
-//const str = document.getElementById('result')
+//const str = document.getElementById('number').value
 
+
+form.onchange = function(e){
+    e.preventDefault()
+    const res = document.getElementById('result')
+    const str = document.getElementById('number').value
+    
+        const ch = str.toString()
+        const hold = reverse(ch)
+        if(hold==ch){
+            res.innerText = 'Yes'
+        }
+        else{
+            res.innerText = 'No'
+        }    
+}
 
 form.onsubmit = function(e){
     e.preventDefault()
